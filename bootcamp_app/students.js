4f1c9f713b2b86ffc19814d2e69f15537342d669
Daniel Xue <danixue@gmail.com>
2021-11-27 02:43:44 -0500

    using javascript to query SQL

---
+++ bootcamp_app/students.js
@@ -0,0 +1,26 @@
+const month = process.argv[2];
+const output = process.argv[3];
+
+const {Pool} = require('pg');
+
+const pool = new Pool({
+  user : 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+  SELECT students.id AS student_id, students.name AS student_name, cohorts.name
+  FROM students 
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  LIMIT 5;
+`)
+.then(res => {
+  // console.log(res.rows);
+  for (let i = 0; i < output; i++) {
+    console.log(`${res.rows[i]["student_name"]} has an id of ${res.rows[i]["student_id"]} and was in the ${res.rows[i]["name"]} cohort`);
+    
+  }
+})
+.catch(err => console.error('query error', err.stack));
